4ffabe22b19016ab44fc4c469cf797785aa61c34
laqieer <laqieer@126.com>
2023-01-14 10:18:56 +0800

    update

--- improve_by_decomp.py
+++ improve_by_decomp.py
@@ -11,7 +11,7 @@ columns = {
         }
 
 prefixs = {
-        "fe6.txt": "https://github.com/StanHash/fe6/blob/cc332ec",
+        "fe6.txt": "https://github.com/StanHash/fe6/blob/161df888",
         "fireemblem8u.txt": "https://github.com/laqieer/fireemblem8u/blob/master"
         }
 
